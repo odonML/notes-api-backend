6c35649527c68aba648991036618ea2aa8b73bef
Odon_ML <odon@integriapp.com>
2021-03-21 13:12:39 -0600

    port

--- index.js
+++ index.js
@@ -84,7 +84,8 @@ app.use((request, response) => {
   });
 });
 
-const PORT = precess.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
